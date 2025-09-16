d1adf63f293095599941dac0012dfa3a981b785f
althafeddy@gmail.com <althafeddy@gmail.com>
2025-09-16 14:54:02 +0800

    LAB-AP-D2/.git/COMMIT_EDITMSG

--- althaf.py
+++ althaf.py
@@ -1,1 +1,14 @@
-print ("hello world")
+# data types 
+# list []
+# boolean(true/false)
+# integer(1,2,3,4,)
+# tuple()
+# set{}
+# dictionary{key:value}
+# operators(+,-,*,/,%,//,**,>,<,==,!=,>=,<=)
+# operator logika(and,or,not)
+# operator binary(&,)
+# assigment(=,+=,-=,*=,/=,%=,//=,**=,&=,)
+# operator identitas(is,is not)
+# operator keanggotaan(in,not in)
+# operator unary(-,not,~)
